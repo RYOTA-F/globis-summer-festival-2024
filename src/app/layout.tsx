68c96cd09148243c2b664533f88330e8d5a562ca
RYOTA-F <ry.fj0304@gmail.com>
2024-07-25 23:40:35 +0900

    :sparkles: Prettier 導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "GLOBIS夏祭り2024",
-  description: "『GLOBIS 夏祭り2024』の特設ページです",
-};
+  title: 'GLOBIS夏祭り2024',
+  description: '『GLOBIS 夏祭り2024』の特設ページです',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ja">
       <body>{children}</body>
     </html>
-  );
+  )
 }
