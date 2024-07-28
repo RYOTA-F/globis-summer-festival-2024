c603ac334780e839f15071be3bdf9512ead46837
RYOTA-F <ry.fj0304@gmail.com>
2024-07-28 16:19:58 +0900

    :sparkles: SectionTemplate 実装

---
+++ src/components/templates/SectionTemplate/SectionTemplate.tsx
@@ -0,0 +1,33 @@
+import { H2 } from '../../../components/elements/heading/H2/H2'
+import { H3 } from '../../../components/elements/heading/H3/H3'
+
+interface SectionTemplateProps {
+  children: React.ReactNode
+  title: string
+  subTitle?: string
+  height?: string
+  backgroundColor?: string
+}
+
+export const ARIA_LABEL = 'sectionTemplate' as const
+
+export function SectionTemplate({
+  children,
+  title,
+  subTitle,
+  height = '100vh',
+  backgroundColor = '#888888',
+}: SectionTemplateProps) {
+  return (
+    <section
+      className={`flex justify-center items-center ${height}  bg-[${backgroundColor}] p-[10%]`}
+      aria-label={ARIA_LABEL}
+    >
+      <div className="w-full text-center">
+        {subTitle && <H3>{subTitle}</H3>}
+        <H2>{title}</H2>
+        <div className="flex justify-center items-center mt-10">{children}</div>
+      </div>
+    </section>
+  )
+}
