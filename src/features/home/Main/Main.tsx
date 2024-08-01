8b949df9cefea2fab8a516e6b68c1cbe569d59df
RYOTA-F <ry.fj0304@gmail.com>
2024-08-02 03:30:13 +0900

    :art: デザイン 修正

--- src/features/home/Main/Main.tsx
+++ src/features/home/Main/Main.tsx
@@ -6,13 +6,13 @@ export const ARIA_LABEL = 'main' as const
 export function MainSection() {
   return (
     <section
-      className="h-[calc(100vh-64px)] w-[100vw] flex flex-col"
+      className="h-[calc(100vh-64px)] w-[100vw] flex flex-col mb-10"
       aria-label={ARIA_LABEL}
     >
       <img src="/summer_festival_2024.jpeg" className="w-full" />
       <div className="bg-sub h-[100px]" />
-      <div className="bg-sub flex-grow relative -z-20">
-        <div className="absolute left-1/2 transform -translate-x-1/2 w-full h-[200%] bg-white rounded-[50%] -z-10"></div>
+      <div className="bg-sub flex-grow relative">
+        <div className="absolute left-1/2 transform -translate-x-1/2 w-full h-[200%] bg-white rounded-[50%]"></div>
       </div>
     </section>
   )
