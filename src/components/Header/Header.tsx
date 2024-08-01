a9611aeee1dc25b63ba8484ee53c6097b762533c
RYOTA-F <ry.fj0304@gmail.com>
2024-08-02 03:18:08 +0900

    :art: Main 修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const ARIA_LABEL = 'header' as const
 export function Header() {
   return (
     <header aria-label={ARIA_LABEL}>
-      <div className="flex items-center h-16 bg-[#f0f0f0] px-6">
+      <div className="flex items-center h-16 bg-sub px-6">
         <Icon src="/logo.png" width={260} height={50} />
       </div>
     </header>
