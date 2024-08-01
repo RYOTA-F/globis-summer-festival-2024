0f1669b7194c5d4105f1b8da47b0ffae3d8a07fb
RYOTA-F <ry.fj0304@gmail.com>
2024-08-02 03:44:13 +0900

    :recycle: Contents 修正

--- src/constants/contents.ts
+++ src/constants/contents.ts
@@ -4,77 +4,77 @@ export const SUB = 'sub' as const
 export const CONTENTS = [
   {
     name: '盆踊り',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: MAIN,
   },
   {
     name: 'お神輿',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: MAIN,
   },
   {
     name: '未成年の主張',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: MAIN,
   },
   {
     name: 'スイカの種飛ばしコンテスト',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: MAIN,
   },
   {
     name: 'ダンス',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: MAIN,
   },
   {
     name: 'ヨーヨーすくい',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
   },
   {
     name: '輪投げ',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
   },
   {
     name: 'スーパーボウルすくい',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
   },
   {
     name: '射的',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
   },
   {
     name: '花火',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
   },
   {
     name: '出会いの始まり',
-    image: '/500.png',
+    image: '/mikoshi.webp',
     description:
       'これは説明文です。これは説明文です。これは説明文です。これは説明文です。これは説明文です。',
     type: SUB,
