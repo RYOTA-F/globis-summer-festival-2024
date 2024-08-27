3b1c87791bb46ad22774f1be01e7488ed0480914
RYOTA-F <ry.fj0304@gmail.com>
2024-08-27 19:18:44 +0900

    :art: Main 画像修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,9 @@ export const HOME_URL = '/#'
 export function Header() {
   return (
     <header className="sticky top-0" aria-label={ARIA_LABEL}>
-      <div className="flex justify-between items-center h-16 bg-w70 px-3 z-10000">
-        <a href="/">
-          <Icon src="/logo.png" width={260} height={50} />
+      <div className="flex justify-between items-center h-12 sm:h-16 bg-w70 px-3 z-10000">
+        <a href="/" className="flex items-center h-full">
+          <Icon src="/logo.png" width={260} height={54} />
         </a>
         <nav className="hidden md:flex justify-end items-center w-full h-full text-blue-600 font-semibold tracking-widest ">
           {NAVIGATION.map((nav) => (
