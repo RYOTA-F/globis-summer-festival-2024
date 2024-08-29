6a9a932f59876c81addfccb19d26dc70a4f8e0c0
RYOTA-F <ry.fj0304@gmail.com>
2024-08-29 21:06:35 +0900

    :art: デザイン修正

--- src/features/access/Route/Route.tsx
+++ src/features/access/Route/Route.tsx
@@ -14,11 +14,11 @@ export function RouteSection() {
       height="auto"
       ariaLabel={ARIA_LABEL}
     >
-      <div className="sm:grid sm:grid-cols-2 sm:gap-10">
+      <div className="sm:grid sm:grid-cols-2 sm:gap-10 -mt-10">
         {ROUTES.map((route) => (
           <div
             key={route.NUMBER}
-            className="col-span-1 flex flex-col items-center [&>label]:block [&>label]:w-[30px] [&>label]:h-[30px] [&>label]:leading-[22px] md:mt-0"
+            className="col-span-1 flex flex-col items-center [&>label]:block [&>label]:w-[30px] [&>label]:h-[30px] [&>label]:leading-[22px] mt-5 md:mt-0"
           >
             <NumberLabel color="bg-blue-500">{route.NUMBER}</NumberLabel>
             <img src={route.SRC} className="mt-3 rounded-2xl" />
