c2b4d1b15de27f808af74c0b5fc6e92056493551
RYOTA-F <ry.fj0304@gmail.com>
2024-08-30 15:58:11 +0900

    :art: Route修正

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,28 +1,30 @@
 export const ROUTES = [
   {
     NUMBER: 1,
-    SRC: './route/route_0.JPG',
+    SRC: './route/route_1.jpg',
     MESSAGE:
       '葛西臨海公園内の「クリスタルビュー」の奥に「ソラミドBBQ場」がございます。',
   },
   {
     NUMBER: 2,
-    SRC: './route/route_1.JPG',
+    SRC: './route/route_2.jpg',
     MESSAGE: '葛西臨海公園に入ったら中央の通路を直進してください。',
   },
   {
     NUMBER: 3,
-    SRC: './route/route_2.JPG',
+    SRC: './route/route_3.jpg',
     MESSAGE: '中央の時計がある広場をさらに直進してください。',
   },
   {
     NUMBER: 4,
-    SRC: './route/route_3.JPG',
-    MESSAGE: 'ガラス張りの建物「クリスタルビュー」の下を通り抜けてください。',
+    SRC: './route/route_4.jpg',
+    MESSAGE:
+      'ガラス張りの建物「クリスタルビュー」の下を通り抜けて、右に曲がってください。',
   },
   {
     NUMBER: 5,
-    SRC: './route/route_4.JPG',
-    MESSAGE: '”右"に曲がり奥の階段から下に降りてください。',
+    SRC: './route/route_5.jpg',
+    MESSAGE:
+      '奥に進むと生垣の先に階段があります。その階段を下に降りてください。',
   },
 ] as const
