1019f556f6e5b03b89cb7025894541ab7ffbabfc
RYOTA-F <ry.fj0304@gmail.com>
2024-08-30 20:54:27 +0900

    :sparkles: OGP 設定

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -2,10 +2,12 @@ const GLOBIS = 'GLOBIS' as const
 const SUMMER_FESTIVAL = '夏祭り' as const
 const YEAR_2024 = '2024' as const
 const TITLE = `${GLOBIS}${SUMMER_FESTIVAL}${YEAR_2024}` as const
+const DESCRIPTION = `G会が主催する夏祭りイベントです` as const
 const COPY = `${TITLE} 実行委員` as const
 
 export const SITE = {
   TITLE,
+  DESCRIPTION,
   GLOBIS,
   SUMMER_FESTIVAL,
   YEAR_2024,
